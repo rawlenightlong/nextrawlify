fb233b25c98e7b10f7e4394177a485fae26cba73
Rawle Becerril <rawlesxm@gmail.com>
2023-04-02 23:08:17 -0400

    youtube margin

--- components/Login.js
+++ components/Login.js
@@ -28,7 +28,7 @@ export default function Login(){
             </a>
         </div>
         <div id="demovideo">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/7GkGq2iRfiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" className="m-auto mt-2" src="https://www.youtube.com/embed/7GkGq2iRfiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
 
     </div>
